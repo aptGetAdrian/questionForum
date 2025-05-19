f268bf23e819b47610a01bb16a88387cdf16ccc9
Adrian <cvetkoadrian@gmail.com>
2025-05-19 07:37:34 +0200

    cleaned up the directories

--- public/javascripts/voting.js
+++ public/javascripts/voting.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const styleElement = document.createElement('style');
-    styleElement.textContent = `
+document.addEventListener("DOMContentLoaded", () => {
+  const styleElement = document.createElement("style");
+  styleElement.textContent = `
       .voted-up { 
         background-color:rgb(21, 135, 21) !important; 
         font-weight: bold !important;
@@ -14,52 +14,55 @@ document.addEventListener('DOMContentLoaded', () => {
         border-radius: 25px;
       }
     `;
-    document.head.appendChild(styleElement);
-    
-    const voteButtons = document.querySelectorAll('.vote-btn');
-    voteButtons.forEach(button => {
-      button.addEventListener('click', async (event) => {
-        const commentId = button.getAttribute('data-comment-id');
-        const userId = button.getAttribute('data-user-id');
-        const voteType = button.getAttribute('data-vote-type');
-        
-        try {
-          const response = await fetch(`/comments/${commentId}/${voteType}/${userId}`, {
-            method: 'POST',
+  document.head.appendChild(styleElement);
+
+  const voteButtons = document.querySelectorAll(".vote-btn");
+  voteButtons.forEach((button) => {
+    button.addEventListener("click", async (event) => {
+      const commentId = button.getAttribute("data-comment-id");
+      const userId = button.getAttribute("data-user-id");
+      const voteType = button.getAttribute("data-vote-type");
+
+      try {
+        const response = await fetch(
+          `/comments/${commentId}/${voteType}/${userId}`,
+          {
+            method: "POST",
             headers: {
-              'Content-Type': 'application/json',
-              'X-Requested-With': 'XMLHttpRequest'
-            }
-          });
-          
-          if (response.ok) {
-            const data = await response.json();
-            console.log('Vote response data:', data); 
-            
-            const scoreElement = document.getElementById(`score-${commentId}`);
-            scoreElement.textContent = "Score: " + data.score;
-            
-            const upvoteBtn = document.querySelector(
-              `.vote-btn[data-comment-id="${commentId}"][data-vote-type="upvote"]`
-            );
-            const downvoteBtn = document.querySelector(
-              `.vote-btn[data-comment-id="${commentId}"][data-vote-type="downvote"]`
-            );
-            
-            upvoteBtn.classList.remove('voted-up');
-            downvoteBtn.classList.remove('voted-down');
-            
-            if (data.userVote === 1) {
-              upvoteBtn.classList.add('voted-up');
-            } else if (data.userVote === -1) {
-              downvoteBtn.classList.add('voted-down');
-            }
-          } else {
-            console.error('Error in voting:', response.statusText);
+              "Content-Type": "application/json",
+              "X-Requested-With": "XMLHttpRequest",
+            },
           }
-        } catch (error) {
-          console.error('Error in voting:', error);
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log("Vote response data:", data);
+
+          const scoreElement = document.getElementById(`score-${commentId}`);
+          scoreElement.textContent = "Score: " + data.score;
+
+          const upvoteBtn = document.querySelector(
+            `.vote-btn[data-comment-id="${commentId}"][data-vote-type="upvote"]`
+          );
+          const downvoteBtn = document.querySelector(
+            `.vote-btn[data-comment-id="${commentId}"][data-vote-type="downvote"]`
+          );
+
+          upvoteBtn.classList.remove("voted-up");
+          downvoteBtn.classList.remove("voted-down");
+
+          if (data.userVote === 1) {
+            upvoteBtn.classList.add("voted-up");
+          } else if (data.userVote === -1) {
+            downvoteBtn.classList.add("voted-down");
+          }
+        } else {
+          console.error("Error in voting:", response.statusText);
         }
-      });
+      } catch (error) {
+        console.error("Error in voting:", error);
+      }
     });
   });
+});
